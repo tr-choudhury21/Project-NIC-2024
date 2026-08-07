1485a27793ba74aa4dc0c8f6f028d8aa54ac024e
Titu Ray Choudhury <tituraychoudhury@gmail.com>
2026-08-07 19:20:46 +0530

    refactor: use constructor injection in jwt filter

--- Project-NIC/src/main/java/com/projectapi/Project_NIC/filter/JwtFilter.java
+++ Project-NIC/src/main/java/com/projectapi/Project_NIC/filter/JwtFilter.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
